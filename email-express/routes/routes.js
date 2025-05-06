c353b3bf3db3800978bb46b07ec12c78ac9609c0
Evandro Acorsi <evandroaf10@hotmail.com>
2025-05-06 11:56:07 -0300

    Adicionado projeto backend

---
+++ email-express/routes/routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+//import ClienteController from '../controllers/ClienteController.js';
+
+const router = express.Router();
+
+//************ API RESTful - Rotas *****************************************//
+
+//-------------- Cliente ---------------------------------------------------//
+
+// router.post('/clientes', ClienteController.createCliente);
+// router.get('/clientes', ClienteController.getAllClientes);
+// router.post('/clientes/editar/:id', ClienteController.editarCliente);
+// router.get('/clientes/excluir/:id', ClienteController.excluirCliente);
+
+export default router;
